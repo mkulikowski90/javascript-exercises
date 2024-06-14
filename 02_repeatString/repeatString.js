dc4826ea43c076a1dc8f65a89eb1c0dd031cb0e2
mkulikowski90 <michalkul987@gmail.com>
2024-06-14 22:41:43 +0200

    finished ex 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
+const repeatString = function(phrase, numOfRepeats) {
+    let stringToReturn = "";
+    
+    if (numOfRepeats < 0){
+        return "ERROR"
+    }
 
+    for(i = 0; i < numOfRepeats; i++){
+        stringToReturn += phrase
+    }
+    return stringToReturn;
 };
 
 // Do not edit below this line
