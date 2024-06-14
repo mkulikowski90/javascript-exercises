9a0947dbe42568472ace70cd10d4173923fdaf0a
mkulikowski90 <michalkul987@gmail.com>
2024-06-14 23:18:08 +0200

    finished ex 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,12 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    const stringLen = string.lenght;
 
+    let stringLetterArray = [...string];
+
+    stringLetterArray.reverse();
+    let reverseString = stringLetterArray.join("")
+
+    return reverseString
 };
 
 // Do not edit below this line
