2c7cc6f563d6ecbb10f2d6290037e1c19618a568
mkulikowski90 <michalkul987@gmail.com>
2024-06-16 20:20:22 +0200

    finished ex 7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,18 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(tempToConvert) {
+  let tempToReturn = (((tempToConvert - 32) * 5/9));
+  
+  tempToReturn = Math.round(tempToReturn * 10) / 10;
+
+  return tempToReturn;
+
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(tempToConvert) {
+  let tempToReturn = (tempToConvert * 9/5) + 32;
+
+  tempToReturn = Math.round(tempToReturn * 10) / 10;
+
+  return tempToReturn;
 };
 
 // Do not edit below this line
