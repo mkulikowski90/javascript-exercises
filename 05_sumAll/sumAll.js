891f48e844eb5ff2a7031646212d366550448b36
mkulikowski90 <michalkul987@gmail.com>
2024-06-16 19:54:58 +0200

    finished ex 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,30 @@
-const sumAll = function() {
+const sumAll = function(firstNum,lastNum) {
+    function isNumber(value) {
+        return typeof value === 'number';
+    }
+    
+    let sum = 0;
 
+    if (!isNumber(firstNum)|| !isNumber(lastNum)){
+        return ("ERROR");
+    }
+
+    if (firstNum <= 0){
+        return ("ERROR");
+    }
+
+    if (lastNum <= firstNum){
+        const temp = firstNum;
+        firstNum = lastNum;
+        lastNum = temp;
+    }
+
+    for(let i = firstNum;firstNum <= lastNum ;i++){
+        sum = sum + i;
+        firstNum++;
+    }
+    console.log(sum);
+    return sum;
 };
 
 // Do not edit below this line
